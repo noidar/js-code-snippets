9a51376bc1bbbd0cfdbafbe01faa5da58628c23d
Radion <radion.balan@sendmoments.de>
2018-07-16 16:41:28 +0300

    add removeEventlistener function

--- event-emitter.js
+++ event-emitter.js
@@ -13,18 +13,50 @@ class Emitter {
 		this.events[type].push(listener);
 	}
 
-
 	dispatch(type) {
 		this.events[type].forEach((listener) => {
 			listener();
 		})
 	}
+
+	// remove a listener 
+	// listener function must be named
+	removeEventListener(type, listener) {
+		if (this.hasEventListener(type)) {
+
+			const index = this.events[type].indexOf(listener);
+
+			// removes function from names events pool
+			if (index !== -1) {
+				this.events[type].splice(index, 1);
+			}
+		}
+	}
+
+	// return a boolean if exist in events obj type of events
+	hasEventListener(type) {
+		return this.events[type];
+	}
+
+
 }
 
 // example usage
 const emt = new Emitter();
 
+const removeUser = () => { console.log('Perform logic to remove user') }
+const removeComments = () => { console.log('remove Comments') }
+
 emt.addEventListener('log', () => console.log('Client log, update settings'));
 emt.addEventListener('log', () => console.log('Clear sesion'));
 
+
 emt.dispatch('log');
+
+// mimic real-world app when needed perform events
+emt.addEventListener('log-out', removeUser);
+emt.addEventListener('log-out', removeComments);
+
+emt.removeEventListener('log-out', removeComments);
+
+emt.dispatch('log-out');
